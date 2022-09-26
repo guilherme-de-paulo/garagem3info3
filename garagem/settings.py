cc3e847f4b10cc95d27c78defae9e169223fb40f
=LocatelliBryann <=bryannlocatelli@gmail.com>
2022-09-26 15:02:56 -0300

    commit

--- garagem/settings.py
+++ garagem/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core",
     "corsheaders",
     "rest_framework",
-    "core",
-    
+
 ]
 
 MIDDLEWARE = [
